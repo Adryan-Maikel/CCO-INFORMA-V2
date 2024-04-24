9f569bc0acee81516c4b8f81f7c8884986ab105a
NGS-Adryan <ngs.adryan@gmail.com>
2024-04-24 10:48:36 -0300

    implementando melhorias
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ static/js/home.js
@@ -0,0 +1,3 @@
+import { set_cookie_operator } from "./scripts.js"
+
+document.getElementById("logout").addEventListener("click", ()=>set_cookie_operator())
--- static/js/login.js
+++ static/js/login.js
@@ -1,6 +1,8 @@
+import { set_cookie_operator } from "./scripts.js"
+
 const operator = window.document.getElementById("operator")
 window.document.querySelector("form").addEventListener("submit", (events)=>{
     // events.preventDefault()
-    document.cookie = `operator=${operator.value}; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/`
+    set_cookie_operator(operator.value)
     events.submitter
 })
